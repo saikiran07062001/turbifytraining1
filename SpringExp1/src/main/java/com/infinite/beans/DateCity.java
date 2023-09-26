275f162d8aa2ba2035cf5139713f2d505e835c66
saikiran07062001 <saikirangudla23@gmail.com>
2023-09-26 23:51:22 +0530

    spring program

---
+++ SpringExp1/src/main/java/com/infinite/beans/DateCity.java
@@ -0,0 +1,26 @@
+package com.infinite.beans;
+
+
+public class DateCity {
+	private String date;
+	private String name;
+	private String city;
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
---
+++ SpringExp1/src/main/java/com/infinite/beans/Main1.java
@@ -0,0 +1,15 @@
+package com.infinite.beans;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main1 {
+	public static void main(String[] arg) {
+		
+	AbstractApplicationContext context =
+			new ClassPathXmlApplicationContext("New.xml");
+
+	DateCity customer = (DateCity) context.getBean("datecity");
+	System.out.println(customer.getDate());
+	System.out.println(customer.getName());
+	context.registerShutdownHook();
+}
+}
